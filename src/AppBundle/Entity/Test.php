68e454beea4cff3bf501f9e154c1b09413f2d5a5
Maciek <maciej.sikorski@valueadd.pl>
2017-08-21 16:53:40 +0200

    Added Test Entity

---
+++ src/AppBundle/Entity/Test.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity(repositoryClass="\AppBundle\Entity\Repository\ProviderRepository")
+ * @ORM\Table(name="test")
+ */
+class Test
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $name;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $text;
+    public function __construct(string $name, string $text)
+    {
+        $this->name = $name;
+        $this->text = $text;
+    }
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    /**
+     * @return mixed
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+    /**
+     * @param mixed $text
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+    }
+}
